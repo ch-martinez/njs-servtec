f27a987779225afcc739745d9c09801e4f73cb54
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-05 16:44:56 -0300

    feat: modelo de roles

---
+++ src/models/role.model.mjs
@@ -0,0 +1,43 @@
+import { pool } from "../config/connectionDB.mjs";
+
+export const getRoleByUserIdFromDB = async (uid) => {
+    const connection = await pool.getConnection()
+    const query = `
+    SELECT
+        r.*
+    FROM
+        roles r
+    INNER JOIN
+        user_has_role uhr ON uhr.role_id = r.role_id
+    INNER JOIN
+        users u ON u.user_id = uhr.user_id
+
+    WHERE u.user_id = ?`
+
+    try {
+        const [[rows]] = await connection.query(query, uid)
+        return rows
+    } catch (error) {
+        console.log('---[ERROR] model/getUserAll: ',error)
+    } finally {
+        if (connection) connection.release()
+    }
+}
+
+export const getAllRolesFromDB = async () => {
+    const connection = await pool.getConnection()
+    const query = `
+    SELECT
+        r.*
+    FROM
+        roles r`
+
+    try {
+        const [rows] = await connection.query(query)
+        return rows
+    } catch (error) {
+        console.log('---[ERROR] model/getAllRolesFromDB: ',error)
+    } finally {
+        if (connection) connection.release()
+    }
+}
