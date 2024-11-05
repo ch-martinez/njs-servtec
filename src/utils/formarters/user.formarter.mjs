0a8b221ef95fbbec15de5e923ac1a66d10260ea1
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-05 16:51:36 -0300

    feat: formateador de usuarios

---
+++ src/utils/formarters/user.formarter.mjs
@@ -0,0 +1,17 @@
+import { dateFull } from "./date.formarter.mjs"
+
+export const userFormarter = (user) => {
+    console.log(user)
+    return {
+        id: user.user_id,
+        name: user.user_name,
+        lastname: user.user_lastname,
+        dni: user.user_dni,
+        email: user.user_email,
+        created_at: dateFull(user.created_at),
+        updated_at: dateFull(user.updated_at),
+
+        status: user.user_status,
+        deleted_at: dateFull(user.user_deleted_at),
+    }
+}
