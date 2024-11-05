b3206875e37867e30648abcee5d7c898aff27b05
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-05 16:44:24 -0300

    feat: configuracion para conexion a la DB

---
+++ src/config/connectionDB.mjs
@@ -0,0 +1,23 @@
+import { createPool } from "mysql2/promise"
+import { config } from "dotenv"
+config()
+
+export const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'servtec',
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0
+})
+
+// Prueba que la conexion a la DB sea correcta
+pool.getConnection()
+    .then(connection => {
+        console.log('>>> DB: Conexion OK')
+        connection.release()
+    })
+    .catch(err => {
+        console.log(`>>> DB: Error al obtener la conexion, ${err}`)
+    })
