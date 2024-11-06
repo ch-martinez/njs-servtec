8cb9ac2a92d629269e36a7c9b3d90dc30e4d7f90
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-06 16:55:41 -0300

    feat: formateador de clientes

---
+++ src/utils/formarters/customer.formarter.mjs
@@ -0,0 +1,29 @@
+import { dateFull } from "./date.formarter.mjs"
+
+export const customerFormarter = (cust) => {
+    return {
+        id: cust.customer_id,
+        name: cust.customer_name,
+        lastname: cust.customer_lastname,
+        email: cust.customer_email,
+        dni: cust.customer_dni,
+        tel: cust.customer_tel,
+        tel_alt: cust.customer_tel_alt,
+        obs: cust.customer_obs,
+        created_at: dateFull(cust.created_at),
+        updated_at: dateFull(cust.updated_at),
+        disabled: cust.disabled == 0 ? false : true,
+        disabled_at: cust.disabled_at,
+        deleted: cust.deleted == 0 ? false : true,
+        deleted_user_id: cust.deleted_user_id,
+        deleted_at: cust.deleted_at
+    }
+}
+
+export const customerListFormarter = (custList) => {
+    let temp = []
+    custList.forEach(cust => {
+        temp.push(customerFormarter(cust))
+    })
+    return temp
+}
