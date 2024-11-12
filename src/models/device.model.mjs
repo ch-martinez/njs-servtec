99f9f6aa342895007d3ae18b2f1f00d33e9e9256
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-11 23:29:51 -0300

    feat: modelo de los dispositivos

---
+++ src/models/device.model.mjs
@@ -0,0 +1,42 @@
+import { pool } from "../config/connectionDB.mjs";
+
+export const getAllDevicesBrandsFromDB = async () => {
+    const connection = await pool.getConnection()
+    const query = `
+    SELECT
+        *
+    FROM
+        devices_brands
+    `
+
+    try {
+        const [rows] = await connection.query(query)
+        console.log(rows)
+        return rows
+    } catch (error) {
+        console.log('---[ERROR] model/getAllDevicesBrandsFromDB: ',error)
+    } finally {
+        if (connection) connection.release()
+    }
+}
+
+export const getAllModelsByBrandIdFromDB = async (bid) => {
+    const connection = await pool.getConnection()
+    const query = `
+    SELECT
+        dm_id, dm_model
+    FROM
+        devices_models
+    WHERE
+        db_id = ?`
+
+    try {
+        const [rows] = await connection.query(query, bid)
+        console.log(rows)
+        return rows
+    } catch (error) {
+        console.log('---[ERROR] model/getAllModelsByBrandIdFromDB: ',error)
+    } finally {
+        if (connection) connection.release()
+    }
+}
