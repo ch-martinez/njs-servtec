f0dd2cd69cce5d2d8ceb93d4c68d02918b086544
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-11 23:31:06 -0300

    feat: formateador de estados de la orden

---
+++ src/utils/formarters/order_status.formarter.mjs
@@ -0,0 +1,36 @@
+const staus_code = {
+    10: "Recepcion: Orden creada e ingreso registrado",
+    11: "Recepcion: Pendiente de envío a taller",
+
+    20: "Taller: Recibe",
+    21: "Taller: En revisión y evaluación",
+    22: "Taller: No reparable",
+
+    30: "Presupuesto: Realizado",
+    31: "Presupuesto: Notificado",
+    32: "Presupuesto: Pendiente de aprobación",
+    33: "Presupuesto: Revision a pedido del cliente",
+    34: "Presupuesto: APROBADO",
+    35: "Presupuesto: RECHAZADO",
+
+    41: "Reparacion: Pendiente",
+    42: "Reparacion: Pendiente repuesto",
+    43: "Reparacion: En curso",
+
+    45: "Reparacion: Parcial",
+    44: "Reparacion: Reparado",
+    45: "Reparacion: No reparado",
+
+    60: "Entrega: Pendiente",
+    61: "Entrega: Pendiente - 7 dias",
+    62: "Entrega: Pendiente - 15 dias",
+    63: "Entrega: Pendiente - 1 Mes",
+    64: "Entrega: Pendiente - 3 Meses",
+
+    70: "Entrega: Entregado - Reparado parcial",
+    71: "Entrega: Entregado - Reparado",
+    72: "Entrega: Entregado - No reparado",
+    73: "Entrega: No entregado - Abandono",
+
+    90: "ORDEN FINALIZADA"
+}
