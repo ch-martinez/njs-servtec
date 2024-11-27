a23bb20258673f777056fd7c608fc8ac2f009201
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-26 21:41:49 -0300

    feat: rutas para nuevo usuario y editar

--- src/routes/user.route.mjs
+++ src/routes/user.route.mjs
@@ -4,7 +4,16 @@ import * as controller from '../controllers/user.controller.mjs'
 const router = Router()
 
 router.get("/all", controller.userAllView)
+
+// Nuevo usuario
 router.get("/new", controller.userNewView)
+router.post("/new", controller.userNew)
+
 router.get("/:id", controller.userView)
+
+router.get("/:id/edit", controller.userEditView)
+router.put("/:id/edit", controller.userEditPut)
+
+router.post("/:id/status", controller.statusUser)
 
 export default router
