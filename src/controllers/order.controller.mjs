9c6007a40628d8525fa3cfd27048942956a84d62
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-26 21:39:15 -0300

    feat: controlador para nueva orden

--- src/controllers/order.controller.mjs
+++ src/controllers/order.controller.mjs
@@ -1,11 +1,11 @@
 import * as db_model from "../models/device.model.mjs"
 import * as order_model from "../models/order.model.mjs"
 import * as cust_model from "../models/customer.model.mjs"
-import * as formarter from "../utils/formarters/order.formarter.mjs"
+import * as order_formarter from "../utils/formarters/order.formarter.mjs"
 import * as cust_formarter from "../utils/formarters/customer.formarter.mjs"
 
 export const ordersView = async (req, res) => {
-    const orders = formarter.orders(await order_model.getAllOrdersFromDB())
+    const orders = order_formarter.orders(await order_model.getAllOrdersFromDB())
 
     const data = {
         title: 'Ordenes',
@@ -17,8 +17,10 @@ export const ordersView = async (req, res) => {
 
 export const orderNewView = async (req, res) => {
 
+    const cid = req.params.cid
+
     const devices_brands = await db_model.getAllDevicesBrandsFromDB()
-    const cust = cust_formarter.customer(await cust_model.getCustomerByIdFromDB(1))
+    const cust = cust_formarter.customer(await cust_model.getCustomerByIdFromDB(cid))
 
     const data = {
         title: 'Nueva orden',
@@ -28,15 +30,38 @@ export const orderNewView = async (req, res) => {
     res.render('pages/order/order_new', { layout: 'layouts/main_layout', data, devices_brands, cust });
 }
 
+export const orderNew = async (req, res) => {
+    const order_data = order_formarter.orderNewPost(req.params.cid, req.body)
+
+    const insertRes = await order_model.insertOrderInDB(order_data)
+
+    if (insertRes.status) {
+        res.send({
+            status: true,
+            msg: "Orden creada con exito!",
+            url: `/order/${insertRes.order_id}`
+        })
+
+    } else {
+        res.send({
+            status: false,
+            msg: "Error al crear la orden!"
+        })
+    }
+}
+
 export const orderDetailView = async (req, res) => {
 
-    const devices_brands = await db_model.getAllDevicesBrandsFromDB()
-    const cust = cust_formarter.customer(await cust_model.getCustomerByIdFromDB(1))
+    const oid = req.params.id
+    const order = order_formarter.order(await order_model.getOrderByFromDB(oid))
+    const customer = cust_formarter.customer(await cust_model.getCustomerByIdFromDB(order.customer_id))
 
     const data = {
         title: 'Orden #CFM-0000598',
         nav: 'order'
     }
 
-    res.render('pages/order/order_detail', { layout: 'layouts/main_layout', data, devices_brands, cust });
+    console.log(order, customer);
+
+    res.render('pages/order/order_detail', { layout: 'layouts/main_layout', data, order, customer });
 }
