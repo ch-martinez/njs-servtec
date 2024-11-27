ba0f22ce3586c29a93c9e0aeb54eddf937e8346c
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-26 21:41:35 -0300

    feat: rutas para nuevo cliente

--- src/routes/order.route.mjs
+++ src/routes/order.route.mjs
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { orderNewView, ordersView, orderDetailView } from "../controllers/order.controller.mjs";
+import { orderNewView, ordersView, orderDetailView, orderNew } from "../controllers/order.controller.mjs";
 
 const router = Router()
 
 router.get('/all', ordersView)
-router.get('/new', orderNewView)
-router.get('/detail', orderDetailView)
+
+router.get('/new/customer/:cid', orderNewView)
+router.post('/new/customer/:cid', orderNew)
+
+router.get('/:id', orderDetailView)
 
 export default router
