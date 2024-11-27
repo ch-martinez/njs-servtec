24f8b4e9b0c4f1ac70fdfe2abc16088a609d2d5b
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-26 21:42:19 -0300

    feat: datos para nuevo usuario

--- src/utils/formarters/user.formarter.mjs
+++ src/utils/formarters/user.formarter.mjs
@@ -1,7 +1,6 @@
 import { dateFull } from "./date.formarter.mjs"
 
 export const user = (user) => {
-    console.log(user)
     return {
         id: user.user_id,
         name: user.user_name,
@@ -11,7 +10,19 @@ export const user = (user) => {
         created_at: dateFull(user.created_at),
         updated_at: dateFull(user.updated_at),
 
-        status: user.user_status,
-        deleted_at: dateFull(user.user_deleted_at),
+        status: (user.user_status == 1),
+        status_at: dateFull(user.user_status_at),
+    }
+}
+
+export const userNewPost = (user) => {
+    return {
+        user_id: user.user_id,
+        user_name: user.user_name,
+        user_lastname: user.user_lastname,
+        user_dni: user.user_dni,
+        user_email: (user.user_email).toLowerCase(),
+
+        role_id: user.role_id
     }
 }
