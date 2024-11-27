e05200e4f9f2be52577ed67a4d2e52e0fd221916
Christian Martinez <chmartinez.dev@gmail.com>
2024-11-26 21:38:11 -0300

    feat: maneja la carga de dispostivos y sus modelos

--- public/scripts/devices.js
+++ public/scripts/devices.js
@@ -16,18 +16,26 @@ const loadOptions = (res) => {
 db_select.addEventListener('change', (e) => {
     const db_id = e.target.value
     axios.get(`/device/api/get_models/${db_id}`)
-         .then(function (response) {
-             console.log(response.data);
-             loadOptions(response.data)
-         })
+         .then(function (response) {loadOptions(response.data)})
 })
 
 
 dm_select.addEventListener('change', (e) => {
-    if (e.target.value == '13646'){
+    if (e.target.value == '1'){
         dm_other.removeAttribute("disabled");
     }else{
         dm_other.setAttribute("disabled", "");
-        dm_other.value = ''
+        dm_other.value = null
     }
 })
+
+
+const prepaid = document.querySelector('#order_prepaid')
+
+if (prepaid) {
+    const pm = document.querySelector('#order_payment_method')
+    prepaid.addEventListener('click', (e) => {
+        e.target.checked ? pm.removeAttribute("disabled") : pm.setAttribute("disabled", "")
+        pm.value = "none"
+    })
+}
