a9f8c9e7c104f574b7b1507f8c5cdb762e03f168
Christian Martinez <chmartinez.dev@gmail.com>
2024-12-16 12:53:34 -0300

    feat: restablecer contraseña, cambiar contraseña

--- src/controllers/user.controller.mjs
+++ src/controllers/user.controller.mjs
@@ -1,10 +1,13 @@
 import * as user_model from "../models/user.model.mjs";
 import * as role_model from "../models/role.model.mjs"
+import * as order_model from "../models/order.model.mjs"
 import * as user_login_model from "../models/user_login.model.mjs"
 
 import * as user_formarter from "../utils/formarters/user.formarter.mjs";
 import * as date_formarter from "../utils/formarters/date.formarter.mjs";
 import * as role_formarter from "../utils/formarters/role.formarter.mjs";
+import * as order_formarter from "../utils/formarters/order.formarter.mjs";
+import * as generator from "../utils/ticket_generator.mjs";
 
 export const userAllView = async (req, res) => {
 
@@ -26,7 +29,7 @@ export const userView = async (req, res) => {
     const user_data = user_formarter.user(await user_model.getUserByIdFromDB(uid))
     const user_login_history = date_formarter.loginHistory(await user_login_model.getUserLoginByIdFromDB(uid))
     const user_role = role_formarter.role(await role_model.getRoleByUserIdFromDB(uid))
-
+    const user_orders = order_formarter.list(await order_model.allUserOrdersDB(uid))
 
     let data = {
         title: 'Detalle de usuario',
@@ -36,7 +39,8 @@ export const userView = async (req, res) => {
     const user = {
         ...user_data,
         role: user_role,
-        login_history: user_login_history
+        login_history: user_login_history,
+        orders: user_orders
     }
 
     res.render('pages/user/user_detail', { layout: 'layouts/main_layout', data, user });
@@ -78,7 +82,6 @@ export const userEditPut = async (req, res) => {
             msg: "Error al actualizar los datos!"
         })
     }
-
 };
 
 export const userNewView = async (req, res) => {
@@ -135,3 +138,58 @@ export const statusUser = async (req, res) => {
         })
     }
 }
+
+
+export const updateUserPasswordView = async (req, res) => {
+
+    const uid = req.params.id
+
+    let data = {
+        title: 'Cambiar contraseña',
+        nav: 'user',
+    };
+
+    res.render('pages/user/user_password_update', { layout: 'layouts/main_layout', data, uid});
+};
+
+export const updateUserPassword = async (req, res) => {
+
+    const user = user_formarter.updateUserPassword(req.params.id, req.body)
+
+    const updateRes = await user_model.updateUserPasswordDB(user)
+
+    if (updateRes.status) {
+        res.send({
+            status: true,
+            msg: "Actualizado con exito!",
+            url: `/user/${user.id}`
+        })
+    }else{
+        res.send({
+            status: false,
+            msg: "Error al actualizar los datos!"
+        })
+    }
+};
+
+export const resetUserPassword = async (req, res) => {
+
+    const pwd = {pwd: generator.randomPwd()}
+
+    const user = user_formarter.updateUserPassword(req.params.id, pwd)
+
+    const updateRes = await user_model.updateUserPasswordDB(user)
+
+    if (updateRes.status) {
+        res.send({
+            status: true,
+            msg: "Contraseña restablecida con exito!",
+            url: `/user/${user.id}`
+        })
+    }else{
+        res.send({
+            status: false,
+            msg: "Error al restablecer contraseña!"
+        })
+    }
+};
