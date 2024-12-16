de858e57974f7a0ff48bb3c0f63e53a5ca079e71
Christian Martinez <chmartinez.dev@gmail.com>
2024-12-16 12:56:09 -0300

    feat: rutas para ordenes de garantia

--- src/routes/order.route.mjs
+++ src/routes/order.route.mjs
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { orderNewView, ordersView, orderDetailView, orderNew } from "../controllers/order.controller.mjs";
+import {
+    orderNewView,
+    ordersView,
+    orderDetailView,
+    orderNew,
+    orderWarrantyNewView,
+    orderWarrantyNew,
+    orderEditView,
+    orderEdit,
+    nextStatus
+} from "../controllers/order.controller.mjs";
 
 const router = Router()
 
@@ -8,6 +18,15 @@ router.get('/all', ordersView)
 router.get('/new/customer/:cid', orderNewView)
 router.post('/new/customer/:cid', orderNew)
 
+router.get('/new/warranty/:oid', orderWarrantyNewView)
+router.post('/new/warranty/:oid', orderWarrantyNew)
+
+
 router.get('/:id', orderDetailView)
+
+router.get('/:oid/edit', orderEditView)
+router.post('/:oid/edit', orderEdit)
+
+router.post('/:id/status', nextStatus)
 
 export default router
