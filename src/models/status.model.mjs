04904b1662d6533b0c81aa4da162935a11b7fc43
Christian Martinez <chmartinez.dev@gmail.com>
2024-12-29 21:41:23 -0300

    refactor: se implementa uuid en las querys

--- src/models/status.model.mjs
+++ src/models/status.model.mjs
@@ -7,7 +7,10 @@ export const getStatusHistoryDB = async (oid) => {
     try {
         const query = `
         SELECT
-            osh.*,
+            BIN_TO_UUID(osh.order_id) AS order_id,
+            BIN_TO_UUID(osh.created_by) AS created_by,
+            osh.created_at,
+            osh.osh_current,
             osc.osc_description,
             u.user_name,
             u.user_lastname
@@ -18,7 +21,7 @@ export const getStatusHistoryDB = async (oid) => {
         INNER JOIN
             users u ON osh.created_by = u.user_id
         WHERE
-            osh.order_id = ?
+            BIN_TO_UUID(osh.order_id) = ?
         ORDER BY osh.created_at DESC`
 
         const [result] = await connection.query(query, oid)
@@ -36,7 +39,11 @@ export const getLastStatusDB = async (oid) => {
     try {
         const query = `
         SELECT
-            osh.*,
+            BIN_TO_UUID(osh.order_id) AS order_id,
+            BIN_TO_UUID(osh.created_by) AS created_by,
+            osh.created_at,
+            osh.osh_current,
+            osh.osc_id,
             osc.osc_description,
             osc.osc_next_status
         FROM
@@ -44,7 +51,7 @@ export const getLastStatusDB = async (oid) => {
         INNER JOIN
             order_status_code osc ON osh.osc_id = osc.osc_id
         WHERE
-            osh.order_id = ? AND osh.osh_current = 1
+            BIN_TO_UUID(osh.order_id) = ? AND osh.osh_current = 1
         `
 
 
@@ -70,8 +77,8 @@ export const insertNextStatusDB = async (data) => {
         //Iniciar transacción
         await connection.beginTransaction()
 
-        const queryUpdate = "UPDATE order_status_history SET osh_current = 0 WHERE order_id = ? AND osh_current = 1;"
-        const queryInsert = `INSERT INTO order_status_history (order_id, osc_id, created_by) VALUES (?, ?, ?)`
+        const queryUpdate = "UPDATE order_status_history SET osh_current = 0 WHERE BIN_TO_UUID(order_id) = ? AND osh_current = 1;"
+        const queryInsert = `INSERT INTO order_status_history (order_id, osc_id, created_by) VALUES (UUID_TO_BIN(?), ?, UUID_TO_BIN(?))`
         const params = [data.order_id, data.next_status, data.user_id]
 
         await connection.query(queryUpdate, data.order_id)
