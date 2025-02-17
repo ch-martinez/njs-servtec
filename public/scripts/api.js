18a25097881a787279fa98c05e128277a4725f3f
Christian Martinez <chmartinez.dev@gmail.com>
2025-02-16 21:02:45 -0300

    feat: axios para form de imagens

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,5 +1,6 @@
 /* Lee el formulario y sus datos */
 const form = document.querySelector('#form')
+const form_multi = document.querySelector('#form_multi')
 
 if (form) {
     form.addEventListener('submit', (e) => {
@@ -11,6 +12,20 @@ if (form) {
         formData.forEach((value, key) => { data[key] = value })
 
         axios.post(url, data)
+             .then(res => {res.data.status ? swal_succes(res.data) : swal_error(res.data)})
+    })
+}
+
+if (form_multi) {
+    form_multi.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const url = form_multi.action
+        const formData = new FormData(form_multi)
+        let data = {}
+
+        formData.forEach((value, key) => { data[key] = value })
+
+        axios.post(url, data, { headers: { 'Content-Type': 'multipart/form-data' }})
              .then(res => {res.data.status ? swal_succes(res.data) : swal_error(res.data)})
     })
 }
